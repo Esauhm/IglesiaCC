0bf44724c22c8e76ab94566ea366757d35ca8e92
DiegoHdez <88221088+Esauhm@users.noreply.github.com>
2025-06-24 15:31:22 -0600

    Add files via upload

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://ministeriocc.com',
+    generateRobotsTxt: true, // Genera también un archivo robots.txt
+    sitemapSize: 5000, // Número máximo de URLs por sitemap
+  };
