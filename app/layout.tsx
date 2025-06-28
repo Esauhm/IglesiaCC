96933b917304a20388244faa29765c1dde2c43db
DiegoHdez <88221088+Esauhm@users.noreply.github.com>
2025-06-28 15:44:25 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
     <html lang="es">
       <head>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
-        <link rel="icon" href="/imagenes/logo1.ico" />
+        <link rel="icon" href="/imagenes/logo1.ico" type="image/x-icon" />
       </head>
       <body className={inter.className}>
         <Header />
