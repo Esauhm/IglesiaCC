05ecb1f7e7c98e3b181644023b0d0b09b2b43c71
DiegoHdez <88221088+Esauhm@users.noreply.github.com>
2025-06-28 16:05:00 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,26 +22,8 @@ export default function RootLayout({
   return (
     <html lang="es">
       <head>
-         <!-- Bootstrap -->
-          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
-        
-          <!-- Favicon para todos los navegadores -->
-          <link rel="icon" href="/imagenes/logo1.ico" type="image/x-icon" />
-          <link rel="shortcut icon" href="/imagenes/logo1.ico" type="image/x-icon" />
-        
-          <!-- Apple Touch Icon (puede ser opcional si quieres dejar solo uno) -->
-          <link rel="apple-touch-icon" sizes="180x180" href="/imagenes/logo1.ico" />
-        
-          <!-- Schema.org para el logo (SEO y Google Knowledge Panel) -->
-          <script type="application/ld+json">
-            {
-              "@context": "https://schema.org",
-              "@type": "Organization",
-              "name": "Ministerio Centro Cristiano",
-              "url": "https://ministeriocc.com",
-              "logo": "https://ministeriocc.com/imagenes/logo1.ico"
-            }
-          </script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
+        <link rel="icon" href="/imagenes/logo1.ico" />
       </head>
       <body className={inter.className}>
         <Header />
